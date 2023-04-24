c3154a56ae505ef36bbfc3c2c2a3bad3e6e7cc2d
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2023-04-24 20:29:18 +0900

    ♻️ :: @Jsonproperty -> @JsonNaming으로 수정

--- report-infrastructure/src/main/kotlin/com/xquare/v2servicereport/common/feign/client/dto/response/UserResponse.kt
+++ report-infrastructure/src/main/kotlin/com/xquare/v2servicereport/common/feign/client/dto/response/UserResponse.kt
@@ -1,21 +1,19 @@
 package com.xquare.v2servicereport.common.feign.client.dto.response
 
-import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.databind.PropertyNamingStrategies
+import com.fasterxml.jackson.databind.annotation.JsonNaming
 import java.time.LocalDate
 import java.util.UUID
 
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy::class)
 data class UserResponse(
     val id: UUID,
     val name: String,
-    @JsonProperty("birth_day")
     val birthDay: LocalDate,
     val grade: Int,
-    @JsonProperty("class_num")
     val classNum: Int,
-    @JsonProperty("profile_file_name")
     val profileFileName: String?,
     val password: String,
-    @JsonProperty("account_id")
     val accountId: String,
     val num: Int
 )
