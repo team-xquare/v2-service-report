765253b0172b69997c3617d53b3c90ab4f171df2
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2023-04-24 21:40:28 +0900

    ♻️ :: 확장 함수로 변경

--- report-infrastructure/src/main/kotlin/com/xquare/v2servicereport/reportimage/domain/ReportImageMapper.kt
+++ report-infrastructure/src/main/kotlin/com/xquare/v2servicereport/reportimage/domain/ReportImageMapper.kt
@@ -1,38 +1,25 @@
 package com.xquare.v2servicereport.reportimage.domain
 
-import com.xquare.v2servicereport.common.annotation.Mapper
 import com.xquare.v2servicereport.report.domain.ReportRepository
 import com.xquare.v2servicereport.report.exceptions.ReportNotFoundException
 import com.xquare.v2servicereport.reportimage.ReportImage
 import org.springframework.data.repository.findByIdOrNull
 
-@Mapper
-class ReportImageMapper(
-    private val reportRepository: ReportRepository,
-) {
+fun ReportImageEntity.toChangeEntityToDomain(): ReportImage {
+    return ReportImage(
+        id = this.id,
+        imageUrl = this.imageUrl,
+        reportId = this.reportEntity.id,
+    )
+}
 
-    fun domainToEntity(reportImage: ReportImage): ReportImageEntity =
-        reportImage.toChangeDomainToEntity()
+fun ReportImage.toChangeDomainToEntity(reportRepository: ReportRepository): ReportImageEntity {
+    val reportEntity = reportRepository.findByIdOrNull(this.reportId)
+        ?: throw ReportNotFoundException
 
-    fun entityToDomain(reportImageEntity: ReportImageEntity): ReportImage =
-        reportImageEntity.toChangeEntityToDomain()
-
-    private fun ReportImageEntity.toChangeEntityToDomain(): ReportImage {
-        return ReportImage(
-            id = this.id,
-            imageUrl = this.imageUrl,
-            reportId = this.reportEntity.id,
-        )
-    }
-
-    private fun ReportImage.toChangeDomainToEntity(): ReportImageEntity {
-        val reportEntity = reportRepository.findByIdOrNull(this.reportId)
-            ?: throw ReportNotFoundException
-
-        return ReportImageEntity(
-            id = this.id,
-            imageUrl = this.imageUrl,
-            reportEntity = reportEntity,
-        )
-    }
+    return ReportImageEntity(
+        id = this.id,
+        imageUrl = this.imageUrl,
+        reportEntity = reportEntity,
+    )
 }
--- report-infrastructure/src/main/kotlin/com/xquare/v2servicereport/reportimage/domain/ReportImagePersistenceAdapter.kt
+++ report-infrastructure/src/main/kotlin/com/xquare/v2servicereport/reportimage/domain/ReportImagePersistenceAdapter.kt
@@ -1,20 +1,23 @@
 package com.xquare.v2servicereport.reportimage.domain
 
 import com.xquare.v2servicereport.common.annotation.Adapter
+import com.xquare.v2servicereport.report.domain.ReportRepository
 import com.xquare.v2servicereport.reportimage.ReportImage
 import com.xquare.v2servicereport.reportimage.spi.ReportImageSpi
 
 @Adapter
 class ReportImagePersistenceAdapter(
+    private val reportRepository: ReportRepository,
     private val reportImageRepository: ReportImageRepository,
-    private val reportImageMapper: ReportImageMapper,
 ) : ReportImageSpi {
 
     override fun saveReportImage(reportImage: ReportImage) {
-        reportImageRepository.save(reportImageMapper.domainToEntity(reportImage))
+        reportImageRepository.save(reportImage.toChangeDomainToEntity(reportRepository))
     }
 
     override fun saveAllReportImage(reportImages: List<ReportImage>) {
-        reportImageRepository.saveAll(reportImages.map(reportImageMapper::domainToEntity))
+        reportImageRepository.saveAll(reportImages.map {
+            it.toChangeDomainToEntity(reportRepository)
+        })
     }
 }
