c0d503ba3e4d45a04a2c2bf51a416de478a13e50
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2023-05-12 00:58:19 +0900

    ⚡️ :: 릴리즈 노트 리스트 조회 API 추가

--- report-application/src/main/kotlin/com/xquare/v2servicereport/releasenote/api/ReleaseNoteApi.kt
+++ report-application/src/main/kotlin/com/xquare/v2servicereport/releasenote/api/ReleaseNoteApi.kt
@@ -1,7 +1,9 @@
 package com.xquare.v2servicereport.releasenote.api
 
 import com.xquare.v2servicereport.releasenote.api.dto.request.DomainCreateReleaseNoteRequest
+import com.xquare.v2servicereport.releasenote.api.dto.request.QueryReleaseNoteList
 
 interface ReleaseNoteApi {
     fun createReleaseNote(domainCreateReleaseNoteRequest: DomainCreateReleaseNoteRequest)
+    fun getReleaseNoteList(): QueryReleaseNoteList
 }
--- report-application/src/main/kotlin/com/xquare/v2servicereport/releasenote/api/impl/ReleaseNoteApiImpl.kt
+++ report-application/src/main/kotlin/com/xquare/v2servicereport/releasenote/api/impl/ReleaseNoteApiImpl.kt
@@ -4,11 +4,15 @@ import com.xquare.v2servicereport.common.annotation.DomainService
 import com.xquare.v2servicereport.releasenote.ReleaseNote
 import com.xquare.v2servicereport.releasenote.api.ReleaseNoteApi
 import com.xquare.v2servicereport.releasenote.api.dto.request.DomainCreateReleaseNoteRequest
+import com.xquare.v2servicereport.releasenote.api.dto.request.QueryReleaseNoteList
+import com.xquare.v2servicereport.releasenote.api.dto.request.QueryReleaseNoteList.ReleaseNoteElement
 import com.xquare.v2servicereport.releasenote.spi.CommandReleaseNoteSpi
+import com.xquare.v2servicereport.releasenote.spi.QueryReleaseNoteSpi
 
 @DomainService
 class ReleaseNoteApiImpl(
     private val commandReleaseNoteSpi: CommandReleaseNoteSpi,
+    private val queryReleaseNoteSpi: QueryReleaseNoteSpi,
 ) : ReleaseNoteApi {
 
     override fun createReleaseNote(domainCreateReleaseNoteRequest: DomainCreateReleaseNoteRequest) {
@@ -21,5 +25,20 @@ class ReleaseNoteApiImpl(
                 bugFixContent = bugFixContent,
             ),
         )
+    }
+
+    override fun getReleaseNoteList(): QueryReleaseNoteList {
+        val releaseNoteList = queryReleaseNoteSpi.queryReleaseNoteList()
+
+        val response = releaseNoteList.map { releaseNote ->
+            ReleaseNoteElement(
+                releaseNoteId = releaseNote.id,
+                releaseVersion = releaseNote.releaseVersion,
+                featureContent = releaseNote.featureContent,
+                bugFixContent = releaseNote.bugFixContent,
+            )
+        }
+
+        return QueryReleaseNoteList(response)
     }
 }
