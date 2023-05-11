2687e37e721f072d7849664672cc86732b9e3f68
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2023-05-12 00:27:36 +0900

    ♻️ :: @NotBlank -> @NotNull로 수정

--- report-infrastructure/src/main/kotlin/com/xquare/v2servicereport/report/web/dto/request/CreateReportRequest.kt
+++ report-infrastructure/src/main/kotlin/com/xquare/v2servicereport/report/web/dto/request/CreateReportRequest.kt
@@ -2,12 +2,13 @@ package com.xquare.v2servicereport.report.web.dto.request
 
 import com.xquare.v2servicereport.report.Category
 import javax.validation.constraints.NotBlank
+import javax.validation.constraints.NotNull
 
 data class CreateReportRequest(
     @field:NotBlank
     val reason: String,
-    @field:NotBlank
+    @field:NotNull
     val category: Category,
-    @field:NotBlank
+    @field:NotNull
     val imageUrls: List<String>,
 )
