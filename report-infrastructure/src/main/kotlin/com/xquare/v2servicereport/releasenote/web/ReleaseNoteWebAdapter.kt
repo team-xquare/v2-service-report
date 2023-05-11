0056d9cbfce4b80a92194bea8e9b920d5e5c9c74
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2023-05-12 00:02:15 +0900

    📑 :: ReleaseNoteAdapter들 추가

---
+++ report-infrastructure/src/main/kotlin/com/xquare/v2servicereport/releasenote/web/ReleaseNoteWebAdapter.kt
@@ -0,0 +1,32 @@
+package com.xquare.v2servicereport.releasenote.web
+
+import com.xquare.v2servicereport.releasenote.api.ReleaseNoteApi
+import com.xquare.v2servicereport.releasenote.api.dto.request.DomainCreateReleaseNoteRequest
+import com.xquare.v2servicereport.releasenote.web.dto.request.CreateReleaseNoteRequest
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
+import javax.validation.Valid
+
+@RestController
+class ReleaseNoteWebAdapter(
+    private val releaseNoteApi: ReleaseNoteApi,
+) {
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/note")
+    fun createReleaseNote(
+        @RequestBody
+        @Valid
+        request: CreateReleaseNoteRequest,
+    ) {
+        val domainCreateReleaseNoteRequest = DomainCreateReleaseNoteRequest(
+            releaseVersion = request.releaseVersion,
+            featureContent = request.featureContent,
+            bugFixContent = request.bugFixContent,
+        )
+        releaseNoteApi.createReleaseNote(domainCreateReleaseNoteRequest)
+    }
+}
