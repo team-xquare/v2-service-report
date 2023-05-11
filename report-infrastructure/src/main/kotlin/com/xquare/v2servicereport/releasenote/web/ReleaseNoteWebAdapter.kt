bcfd415d054d1d651ae80945be3955d5f9fa37b4
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2023-05-12 00:16:26 +0900

    ♻️ :: @RequestMapping으로 URL 수정

--- report-infrastructure/src/main/kotlin/com/xquare/v2servicereport/releasenote/web/ReleaseNoteWebAdapter.kt
+++ report-infrastructure/src/main/kotlin/com/xquare/v2servicereport/releasenote/web/ReleaseNoteWebAdapter.kt
@@ -6,17 +6,19 @@ import com.xquare.v2servicereport.releasenote.web.dto.request.CreateReleaseNoteR
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 import javax.validation.Valid
 
+@RequestMapping("/note")
 @RestController
 class ReleaseNoteWebAdapter(
     private val releaseNoteApi: ReleaseNoteApi,
 ) {
 
     @ResponseStatus(HttpStatus.CREATED)
-    @PostMapping("/note")
+    @PostMapping
     fun createReleaseNote(
         @RequestBody
         @Valid
