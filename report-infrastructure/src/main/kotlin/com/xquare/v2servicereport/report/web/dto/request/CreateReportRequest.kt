66d237fa6ed319844b1e4e3be66d3e4a220aabab
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2023-05-12 00:00:29 +0900

    ⚡️ :: @NotBlank 추가

--- report-infrastructure/src/main/kotlin/com/xquare/v2servicereport/report/web/dto/request/CreateReportRequest.kt
+++ report-infrastructure/src/main/kotlin/com/xquare/v2servicereport/report/web/dto/request/CreateReportRequest.kt
@@ -1,9 +1,13 @@
 package com.xquare.v2servicereport.report.web.dto.request
 
 import com.xquare.v2servicereport.report.Category
+import javax.validation.constraints.NotBlank
 
 data class CreateReportRequest(
+    @field:NotBlank
     val reason: String,
+    @field:NotBlank
     val category: Category,
+    @field:NotBlank
     val imageUrls: List<String>,
 )
