978095146e594556a0c8b2a10a43da4d924fd994
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2023-06-11 19:10:57 +0900

    ♻️ :: 리턴 타입 명시 제거

--- report-infrastructure/src/main/kotlin/com/xquare/v2servicereport/releasenote/domain/ReleaseNoteMapper.kt
+++ report-infrastructure/src/main/kotlin/com/xquare/v2servicereport/releasenote/domain/ReleaseNoteMapper.kt
@@ -2,20 +2,16 @@ package com.xquare.v2servicereport.releasenote.domain
 
 import com.xquare.v2servicereport.releasenote.ReleaseNote
 
-fun ReleaseNoteEntity.toChangeEntityToDomain(): ReleaseNote {
-    return ReleaseNote(
-        id = this.id,
-        releaseVersion = this.releaseVersion,
-        featureContent = this.featureContent,
-        bugFixContent = this.bugFixContent,
-    )
-}
+fun ReleaseNoteEntity.toChangeEntityToDomain() = ReleaseNote(
+    id = this.id,
+    releaseVersion = this.releaseVersion,
+    featureContent = this.featureContent,
+    bugFixContent = this.bugFixContent,
+)
 
-fun ReleaseNote.toChangeDomainToEntity(): ReleaseNoteEntity {
-    return ReleaseNoteEntity(
-        id = this.id,
-        releaseVersion = this.releaseVersion,
-        featureContent = this.featureContent,
-        bugFixContent = this.bugFixContent,
-    )
-}
+fun ReleaseNote.toChangeDomainToEntity() = ReleaseNoteEntity(
+    id = this.id,
+    releaseVersion = this.releaseVersion,
+    featureContent = this.featureContent,
+    bugFixContent = this.bugFixContent,
+)
--- report-infrastructure/src/main/kotlin/com/xquare/v2servicereport/report/domain/ReportMapper.kt
+++ report-infrastructure/src/main/kotlin/com/xquare/v2servicereport/report/domain/ReportMapper.kt
@@ -2,22 +2,18 @@ package com.xquare.v2servicereport.report.domain
 
 import com.xquare.v2servicereport.report.Report
 
-fun ReportEntity.toChangeEntityToDomain(): Report {
-    return Report(
-        id = this.id,
-        userId = this.userId,
-        reason = this.reason,
-        category = this.category,
-        createdAt = this.createdAt
-    )
-}
+fun ReportEntity.toChangeEntityToDomain() = Report(
+    id = this.id,
+    userId = this.userId,
+    reason = this.reason,
+    category = this.category,
+    createdAt = this.createdAt,
+)
 
-fun Report.toChangeDomainToEntity(): ReportEntity {
-    return ReportEntity(
-        id = this.id,
-        userId = this.userId,
-        reason = this.reason,
-        category = this.category,
-        createdAt = this.createdAt
-    )
-}
+fun Report.toChangeDomainToEntity() = ReportEntity(
+    id = this.id,
+    userId = this.userId,
+    reason = this.reason,
+    category = this.category,
+    createdAt = this.createdAt,
+)
