7695b607c34fb56bf4777dd97afbe597d044f859
이정호 <dlwjdgh1209@dsm.hs.kr>
2023-06-13 08:36:37 +0900

    ⚡️ :: id로 유저 정보 조회 Feign 추가

--- report-infrastructure/src/main/kotlin/com/xquare/v2servicereport/common/feign/client/UserClientImpl.kt
+++ report-infrastructure/src/main/kotlin/com/xquare/v2servicereport/common/feign/client/UserClientImpl.kt
@@ -1,13 +1,32 @@
 package com.xquare.v2servicereport.common.feign.client
 
+import com.xquare.v2servicereport.user.User
 import com.xquare.v2servicereport.user.spi.UserSpi
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Component
 import java.util.UUID
 
 @Component
-class UserClientImpl : UserSpi {
+class UserClientImpl(
+    private val userClient: UserClient,
+) : UserSpi {
 
     override fun getCurrentUserId(): UUID =
         UUID.fromString(SecurityContextHolder.getContext().authentication.name)
+
+    override fun getUserByUserId(userId: UUID): User {
+        val user = userClient.getUserByUserId(userId)
+
+        return User(
+            id = user.id,
+            accountId = user.accountId,
+            password = user.password,
+            name = user.name,
+            grade = user.grade,
+            classNum = user.classNum,
+            num = user.num,
+            birthDay = user.birthDay,
+            profileFileName = user.profileFileName,
+        )
+    }
 }
