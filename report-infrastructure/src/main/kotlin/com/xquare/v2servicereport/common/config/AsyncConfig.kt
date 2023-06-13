4e41f4cadc12d8199b6d712fbfaae67930852343
이정호 <dlwjdgh1209@dsm.hs.kr>
2023-06-14 01:19:35 +0900

    ⚡️ :: AsyncConfig 추가

---
+++ report-infrastructure/src/main/kotlin/com/xquare/v2servicereport/common/config/AsyncConfig.kt
@@ -0,0 +1,8 @@
+package com.xquare.v2servicereport.common.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.annotation.EnableAsync
+
+@EnableAsync
+@Configuration
+class AsyncConfig
