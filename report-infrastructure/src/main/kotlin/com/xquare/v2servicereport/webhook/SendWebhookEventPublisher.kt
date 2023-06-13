9ebe86192b60b117c4731891c4830effcee7168e
이정호 <dlwjdgh1209@dsm.hs.kr>
2023-06-14 01:22:55 +0900

    ⚡️ :: Slack 메세지 EventListener 처리

---
+++ report-infrastructure/src/main/kotlin/com/xquare/v2servicereport/webhook/SendWebhookEventPublisher.kt
@@ -0,0 +1,15 @@
+package com.xquare.v2servicereport.webhook
+
+import com.xquare.v2servicereport.webhook.spi.SendWebhookSpi
+import org.springframework.context.ApplicationEventPublisher
+import org.springframework.stereotype.Component
+
+@Component
+class SendWebhookEventPublisher(
+    private val applicationEventPublisher: ApplicationEventPublisher,
+) : SendWebhookSpi {
+
+    override fun sendWebhookEvent(slackReport: SlackReport) {
+        applicationEventPublisher.publishEvent(slackReport)
+    }
+}
