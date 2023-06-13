050f70ba519f84a3c4873225122140a3871b31c6
이정호 <dlwjdgh1209@dsm.hs.kr>
2023-06-14 01:22:29 +0900

    ♻️ :: sendWebhookEvent로 메소드명 변경

--- report-application/src/main/kotlin/com/xquare/v2servicereport/report/api/impl/ReportApiImpl.kt
+++ report-application/src/main/kotlin/com/xquare/v2servicereport/report/api/impl/ReportApiImpl.kt
@@ -43,7 +43,7 @@ class ReportApiImpl(
             commandReportImageSpi.saveAllReportImage(reportImages)
         }
 
-        sendWebhookSpi.sendReportMessageToSlack(
+        sendWebhookSpi.sendWebhookEvent(
             SlackReport(
                 userName = userSpi.getUserByUserId(userId).name,
                 reason = reason,
--- report-application/src/main/kotlin/com/xquare/v2servicereport/webhook/spi/SendWebhookSpi.kt
+++ report-application/src/main/kotlin/com/xquare/v2servicereport/webhook/spi/SendWebhookSpi.kt
@@ -3,5 +3,5 @@ package com.xquare.v2servicereport.webhook.spi
 import com.xquare.v2servicereport.webhook.SlackReport
 
 interface SendWebhookSpi {
-    fun sendReportMessageToSlack(slackReport: SlackReport)
+    fun sendWebhookEvent(slackReport: SlackReport)
 }
