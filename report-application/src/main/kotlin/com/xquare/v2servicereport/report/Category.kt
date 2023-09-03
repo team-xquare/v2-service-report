67fc16ff5dbdb70bb53a31feda0c5d71f5ee6830
이정호 <dlwjdgh1209@dsm.hs.kr>
2023-09-03 10:07:16 +0900

    ⚡️ :: 카테고리 추가

--- report-application/src/main/kotlin/com/xquare/v2servicereport/report/Category.kt
+++ report-application/src/main/kotlin/com/xquare/v2servicereport/report/Category.kt
@@ -2,4 +2,5 @@ package com.xquare.v2servicereport.report
 
 enum class Category {
     ALL, APPLICATION, FEED, HOME, SCHEDULE,
+    PICNIC, ATTENDANCE, CHANGE_STUDENT, CHANGE_DIRECTOR, WEEKEND_MEAL
 }
