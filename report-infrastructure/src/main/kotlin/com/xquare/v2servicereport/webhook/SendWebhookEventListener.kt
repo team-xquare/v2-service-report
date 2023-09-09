d0b44dc0e0f646272ccb7bd7b21008cb54d0d7c4
이정호 <dlwjdgh1209@dsm.hs.kr>
2023-09-09 19:23:59 +0900

    ♻️ :: ktlint

--- report-infrastructure/src/main/kotlin/com/xquare/v2servicereport/webhook/SendWebhookEventListener.kt
+++ report-infrastructure/src/main/kotlin/com/xquare/v2servicereport/webhook/SendWebhookEventListener.kt
@@ -7,8 +7,6 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.event.EventListener
 import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Component
-import org.springframework.transaction.event.TransactionPhase
-import org.springframework.transaction.event.TransactionalEventListener
 
 @Component
 class SendWebhookEventListener(
