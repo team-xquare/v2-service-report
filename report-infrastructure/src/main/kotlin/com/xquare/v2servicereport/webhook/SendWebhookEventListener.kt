f09652ca46cec04472dd561d18ee3182426f02d0
이정호 <101508190+jeongho1209@users.noreply.github.com>
2023-09-10 12:25:37 +0900

    🔀 :: (#238) EventListener로 분리 (#18)
    🔀 :: (#238) EventListener로 분리 (#18)

--- report-infrastructure/src/main/kotlin/com/xquare/v2servicereport/webhook/SendWebhookEventListener.kt
+++ report-infrastructure/src/main/kotlin/com/xquare/v2servicereport/webhook/SendWebhookEventListener.kt
@@ -1,18 +1,18 @@
 package com.xquare.v2servicereport.webhook
 
-import com.xquare.v2servicereport.webhook.spi.SendWebhookSpi
 import net.gpedro.integrations.slack.SlackApi
 import net.gpedro.integrations.slack.SlackAttachment
 import net.gpedro.integrations.slack.SlackMessage
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.event.EventListener
 import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Component
 
 @Component
-class SendWebhookAdapter(
+class SendWebhookEventListener(
     @Value("\${webhook.url}")
     private val webhookUrl: String,
-) : SendWebhookSpi {
+) {
 
     companion object {
         private const val REPORT_MESSAGE = "버그 제보 발생"
@@ -20,11 +20,12 @@ class SendWebhookAdapter(
         private const val REPORT_CATEGORY = "카테고리"
         private const val REPORT_USER_NAME = "제보자"
         private const val MESSAGE_COLOR = "#e62e2e"
-        private const val FALLBACK = "Required plain-text summary of the attachment"
+        private const val FALLBACK = "새로운 버그제보가 도착했습니다."
     }
 
     @Async
-    override fun sendReportMessageToSlack(slackReport: SlackReport) {
+    @EventListener
+    fun sendReportMessageToSlack(slackReport: SlackReport) {
         val slackAttachment = SlackAttachment().apply { createSlackAttachment(getReportReason(slackReport)) }
         val slackMessage = SlackMessage("").apply {
             addAttachments(slackAttachment)
---
+++ report-infrastructure/src/main/kotlin/com/xquare/v2servicereport/webhook/SendWebhookEventPublisher.kt
@@ -0,0 +1,14 @@
+package com.xquare.v2servicereport.webhook
+
+import com.xquare.v2servicereport.webhook.spi.SendWebhookSpi
+import org.springframework.context.ApplicationEventPublisher
+import org.springframework.stereotype.Component
+
+@Component
+class SendWebhookEventPublisher(
+    private val applicationEventPublisher: ApplicationEventPublisher,
+) : SendWebhookSpi {
+    override fun sendReportMessageToSlack(slackReport: SlackReport) {
+        applicationEventPublisher.publishEvent(slackReport)
+    }
+}
